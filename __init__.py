813a0e7119d24a44553b543d7dbdc9e4b11033de
Anton Azarov <a.azarov@diagnoptics.com>
2018-05-15 13:42:00 +0200

    status 20180515

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
-_strTest = "just testing"
+#usr/bin/python
+"""
+Python on Steroids / Python of Steel
+"""
+
+__author__ = "Anton Azarov"
+__copyright__ = "(c) 2014-2018 Diagnoptics Technologies B.V."
+__license__ = "GPL"
+__version__ = "0.1.20180515"
+__date__ = "15-05-2018"
+__status__ = "Development"
+__maintainer__ = "a.azarov@diagnoptics.com"
